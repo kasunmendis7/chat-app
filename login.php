d02c8659e129f7f156c0cf3ba7b38422348d8066
kasunmendis7 <tkomendis@gmail.com>
2024-12-08 19:59:50 +0530

    styling users list page

--- login.php
+++ login.php
@@ -9,39 +9,24 @@
 </head>
 <body>
     <div class="wrapper">
-        <section class="form signup">
+        <section class="form login">
             <header>Real-time Chat App</header>
             <form action="#">
                 <div class="error-txt">This is an error message!</div>
-                <div class="name-details">
                     <div class="field input">
-                        <label for="">First Name:</label>
-                        <input type="text" placeholder="First Name...">
-                    </div>
-                    <div class="field input">
-                        <label for="">Last Name:</label>
-                        <input type="text" placeholder="Last Name...">
-                    </div>
-                </div>
-                    <div class="field input">
-                        <label for="">Email Address:</label>
+                        <label for="">Email Address:</label>    
                         <input type="text" placeholder="Enter your email...">
                     </div>
                     <div class="field input">
                         <label for="">Password:</label>
                         <input type="text" placeholder="Password...">
                         <i class="fas fa-eye"></i>
-
-                    </div>
-                    <div class="field image">
-                        <label for="">Select Image</label>
-                        <input type="file" >
                     </div>
                     <div class="field button">
-                        <input type="submit" placeholder="Continue to Chat">
+                        <input type="submit" placeholder="Login to Chat">
                     </div>
             </form>
-            <div class="link">Already has an account? <a href="">Login Now</a></div>
+            <div class="link">Don't have an account? <a href="">SignUp Now</a></div>
         </section>
     </div>
 </body>
