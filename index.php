c4e5eff61aa602221cca0393c7895d201833dcf5
kasunmendis7 <tkomendis@gmail.com>
2024-12-08 18:58:37 +0530

    added sign up form with stylings

--- index.php
+++ index.php
@@ -5,9 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Real-time Chat App</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
 </head>
 <body>
-    
+    <div class="wrapper">
+        <section class="form signup">
+            <header>Real-time Chat App</header>
+            <form action="#">
+                <div class="error-txt">This is an error message!</div>
+                <div class="name-details">
+                    <div class="field input">
+                        <label for="">First Name:</label>
+                        <input type="text" placeholder="First Name...">
+                    </div>
+                    <div class="field input">
+                        <label for="">Last Name:</label>
+                        <input type="text" placeholder="Last Name...">
+                    </div>
+                </div>
+                    <div class="field input">
+                        <label for="">Email Address:</label>
+                        <input type="text" placeholder="Enter your email...">
+                    </div>
+                    <div class="field input">
+                        <label for="">Password:</label>
+                        <input type="text" placeholder="Password...">
+                    </div>
+                    <div class="field image">
+                        <label for="">Select Image</label>
+                        <input type="file" >
+                    </div>
+                    <div class="field button">
+                        <input type="submit" placeholder="Continue to Chat">
+                    </div>
+            </form>
+            <div class="link">Already has an account? <a href="">Login Now</a></div>
+        </section>
+    </div>
 </body>
 </html>
